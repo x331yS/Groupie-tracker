31d37f44550bcefa18e28f96571b4946381dd50b
Totole2108 <anatole.thien@ynov.com>
2021-03-02 10:53:45 +0100

    [EDIT] CSS working

--- main.go
+++ main.go
@@ -10,11 +10,12 @@ import (
 func main() {
 	http.HandleFunc("/", gt_web.IndexHandler)
 	http.HandleFunc("/about/", gt_web.AboutHandler)
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
 	fmt.Println("Listening at localhost:1111\nHttp Status :", http.StatusOK)
-	gt_web.Openbrowser("http://localhost:1111")
+//	gt_web.Openbrowser("http://localhost:1111")
 	err := http.ListenAndServe(":1111", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
